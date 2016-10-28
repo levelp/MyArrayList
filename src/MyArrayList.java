486a9bbf4261d466f94e69ba66859f3fd36ab262
Denis Stepulenok <super.denis@gmail.com>
2016-10-28 21:53:41 +0300

    Реализовали операции добавления add, получения размера size и получения значения элементов get

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,6 +1,40 @@
 public class MyArrayList {
 
-    public static void main(String[] args) {
+    private String[] data = {};
 
+    public static void main(String[] args) {
+        MyArrayList list = new MyArrayList();
+        // TDD: Test Driven Development
+        // Добавление элемента в конец и по индексу
+        list.add("Первая строка");
+        System.out.println("list.data[0] = " + list.data[0]);
+        list.add("Вторая строка");
+        System.out.println("list.data[1] = " + list.data[1]);
+        // Вывод текущего количества элементов
+        System.out.println("list.size() = " + list.size());
+        list.add("Третья строка");
+        // Вывод всех строк
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println("list.get(" + i + ") = " +
+                    list.get(i));
+        }
+    }
+
+    private String get(int i) {
+        return data[i];
+    }
+
+    private int size() {
+        return data.length;
+    }
+
+    private void add(String s) {
+        // Добавляем в массив строк
+        String[] newData = new String[data.length + 1];
+        for (int i = 0; i < data.length; i++) {
+            newData[i] = data[i];
+        }
+        newData[data.length] = s;
+        data = newData;
     }
 }
